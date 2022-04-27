383ba8fe217ecd6dd79cc612b7b2849313067015
sirmodok <sirmodok@gmail.com>
2022-04-27 21:14:18 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,9 @@
-module "s3_bucket" {
-  source = "app.terraform.io/walter-terraform/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/walter-terraform/s3-bucket/aws"
+  version = "2.8.0"
   bucket_prefix = "walter"
   acl    = "private"
-
-  versioning = {
+    versioning = {
     enabled = true
   }
-
 }
